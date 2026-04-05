bded0c0bcf886c7f0e1ad0eb7d9699e0ff5e9589
Shahil-Dev <alfee9006@gmail.com>
2026-04-05 21:45:03 +0600

    Create Doctor Controller

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,3 +1,22 @@
+import { Request, Response } from "express";
+import { CatchAsync } from "../../Shared/CatchAsync";
+import { UserService } from "./user.service";
+import { sendResponse } from "../../Shared/sendResponse";
+import status from "http-status";
+
+const createDoctor = CatchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+  const result = await UserService.createDoctor(payload);
+
+  sendResponse(res, {
+    httpStatusCode: status.CREATED,
+    success: true,
+    message: "Doctor created successfully",
+    data: result,
+  });
+});
+
 export const UserController = {
-    // Add controller methods here
-    };
+  // Add controller methods here
+  createDoctor,
+};
